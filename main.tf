44b71f81a0cd0a19f8f0bce1fb4163ebbdac2eb0
Monica Thamiris Silva Ferreira <monica.tsf@gmail.com>
2025-04-16 23:36:12 -0300

    feat: ajustando nome bucket s3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-fiapeats"
+    bucket  = "terraform-fiapeats-videos"
     key     = "state/fiapeatsbucket/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
